caa45c090b5113f6a42a691b71c676a505863ee9
NandawulaJoan <jnandawula77@gmail.com>
2024-07-07 20:57:33 +0300

    adding work

--- test/test.js
+++ test/test.js
@@ -22,7 +22,7 @@ describe('Multiply', function() {
             expect(c).to.equal(4)
             })
 
-              //test cycle 2
+              //test cycle 3
         it('should return 9 when multiplying 3 and 3', () => {
             var a = 3
             var b = 3
@@ -31,6 +31,27 @@ describe('Multiply', function() {
         
             expect(c).to.equal(9)
             })
+
+                  //test cycle 4
+        it('should return 16 when multiplying 4 and 4', () => {
+            var a = 4
+            var b = 4
+        
+            var c = multiply(a,b)
+        
+            expect(c).to.equal(16)
+            })
+
+                         //test cycle 5
+        it('should return 1035 when multiplying 23 and 45', () => {
+            var a = 23
+            var b = 45
+        
+            var c = multiply(a,b)
+        
+            expect(c).to.equal(1035)
+            })
+
 
   
 
